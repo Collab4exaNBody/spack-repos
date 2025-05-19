ba67cc724a8a6763206ef2ed2e55aa83d18cb0c4
Paul Lafourcade <paul.lafourcade@cea.fr>
2025-05-19 15:55:24 +0200

    Wrong version in dependence definition

--- packages/exastamp/package.py
+++ packages/exastamp/package.py
@@ -13,7 +13,7 @@ class Exastamp(CMakePackage):
     depends_on("cmake@3.27.9")
     depends_on("yaml-cpp@0.6.3")
     depends_on("openmpi")
-    depends_on("exanbody@v2.0.2", when="@3.7.0-rdev")
+    depends_on("exanbody@v2.0.2", when="@3.7.0")
     depends_on("exanbody+contribs")
     depends_on("exanbody+cuda", when="+cuda")
 #    depends_on("onika+cuda", when="+cuda")
